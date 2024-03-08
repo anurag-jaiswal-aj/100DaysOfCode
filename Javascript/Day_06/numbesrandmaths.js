a8d57c06648793b8ed5f05dbca3e437197f0d80c
Anurag Jaiswal <144160916+anurag-jaiswal-aj@users.noreply.github.com>
2024-03-08 20:28:03 +0530

    Update numbesrandmaths.js

--- Javascript/Day_06/numbesrandmaths.js
+++ Javascript/Day_06/numbesrandmaths.js
@@ -1,20 +1,28 @@
 const score = 400
 // console.log(score);
 
+
 const balance = new Number(100)
 // console.log(balance);
 
 // console.log(balance.toString().length);
 // console.log(balance.toFixed(1));
 
+
 const otherNumber = 123.8966
 
 // console.log(otherNumber.toPrecision(4));
 
+
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString('en-IN'));
 
-// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+
+
+
+
+
 
 // console.log(Math);
 // console.log(Math.abs(-4));
