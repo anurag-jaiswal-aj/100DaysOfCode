f2a7b777f72a80f442673ef96074491459784312
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-11 19:15:27 +0530

    .

--- Javascript/Day_05/string.js
+++ Javascript/Day_05/string.js
@@ -4,12 +4,13 @@ const repoCount = 50
 ////console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+  
 
 const gameName = new String('anurag-aj-com')
 
 
 // console.log(gameName[0]);
-// console.log(gameName.__proto__);
+// console.log(gameName.__proto__);   
 
 
 // console.log(gameName.length);
