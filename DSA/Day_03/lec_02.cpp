33fc71add7c5d1f30151a4c00f0f73b7aba39da4
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-24 17:08:33 +0530

    .

--- DSA/Day_03/lec_02.cpp
+++ DSA/Day_03/lec_02.cpp
@@ -20,25 +20,37 @@ using namespace std;
 
 
 
-int main(){
-    int n;
-    cin >> n;
+// -----------------------------------------------
 
 
-    int i = 2;
 
-    while (i<n){
-        if (n%i ==0)
-        {
-            cout<< "Not Prime for "<< i <<endl;
-        }
-        else{
-            cout<< "Prime for "<< i <<endl;
-        }
-        i = i+1;
-    }
+// int main(){
+//     int n;
+//     cin >> n;
+
+
+//     int i = 2;
+
+//     while (i<n){
+//         if (n%i ==0)
+//         {
+//             cout<< "Not Prime for "<< i <<endl;
+//         }
+//         else{
+//             cout<< "Prime for "<< i <<endl;
+//         }
+//         i = i+1;
+//     }
     
-}
+// }
+
+
+
+// -----------------------------------------------
+
+
+
+
 
 
 
