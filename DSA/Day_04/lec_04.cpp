52b3b5e706d71b24813a9d8742334ad5a69dd4a9
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-24 19:42:24 +0530

    .

--- DSA/Day_04/lec_04.cpp
+++ DSA/Day_04/lec_04.cpp
@@ -129,13 +129,11 @@
 // #include <iostream>
 // using namespace std;
 
-
 // int main()
 // {
 //     int n;
 //     cout << "Enter the number of lines:";
 //     cin >> n;
-
 
 //     int num = 1;
 //     int row = 1;
@@ -157,5 +155,31 @@
 
 // }
 
+// -----------------------Pattren-----------------------------------
 
+#include <iostream>
+using namespace std;
 
+int main()
+{
+
+    int n;
+    cout << "Enter the value of n:";
+    cin >> n;
+
+    int i = 1;
+
+    while (i <= n)
+    {
+        int j = 1;
+        int num = i;
+        while (j <= i)
+        {
+            cout << num << " ";
+            num = num + 1;
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+}
