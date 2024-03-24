37df9912e2020ab0f2230cf7587ac22686d65bb7
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-24 19:19:34 +0530

    .

--- DSA/Day_04/lec_04.cpp
+++ DSA/Day_04/lec_04.cpp
@@ -5,18 +5,18 @@
 //     int n ;
 //     cin>> n;
 
-//     int i= 1;
+//     int row= 1;
 
-//     while (i<=n){
+//     while (row<=n){
 
-//         int j = 1;
-//         while(j<=n){
-//             cout<< j;
-//             j = j +1;
+//         int col = 1;
+//         while(col<=n){
+//             cout<< col;
+//             col = col +1;
 //         }
 
 //         cout<<endl;
-//         i = i+1;
+//         row = row+1;
 //     }
 
 // }
@@ -30,18 +30,18 @@
 //     int n ;
 //     cin>> n;
 
-//     int i= n;
+//     int row= n;
 
-//     while (i>=1){
+//     while (row>=1){
 
-//         int j = n;
-//         while(j>=1){
-//             cout<< j;
-//             j = j - 1;
+//         int col = n;
+//         while(col>=1){
+//             cout<< col;
+//             col = col - 1;
 //         }
 
 //         cout<<endl;
-//         i = i - 1;
+//         row = row - 1;
 //     }
 
 // }
@@ -56,22 +56,22 @@
 //     int n;
 //     cin >> n;
 
-//     int i = 1;
+//     int row = 1;
 //     int count = 1;
 
-//     while (i <= n)
+//     while (row <= n)
 //     {
 
-//         int j = 1;
-//         while (j <= n)
+//         int col = 1;
+//         while (col <= n)
 //         {
 //             cout<<count<<"  ";
 //             count = count + 1;
-//             j = j + 1;
+//             col = col + 1;
 //         }
 
 //         cout << endl;
-//         i = i + 1;
+//         row = row + 1;
 //     }
 // }
 
@@ -85,48 +85,77 @@
 //     int n;
 //     cin >> n;
 
-//     int i = 1;
-//     while (i <= n)
+//     int row = 1;
+//     while (row <= n)
 //     {
-//         int j = 1;
-//         while (j <= i)
+//         int col = 1;
+//         while (col <= row)
 //         {
 //             cout << "*";
-//             j = j + 1;
+//             col = col + 1;
 //         }
 //         cout << endl;
-//         i = i + 1;
+//         row = row + 1;
 //     }
 // }
 
+// -----------------------Pattren-----------------------------------
 
+// #include <iostream>
+// using namespace std;
 
+// int main(){
+//     int n ;
+//     cin>>n;
 
+//     int row = 1;
+//     while (row<=n)
+//     {
+//         int col = 1;
+//         while (col<=row)
+//         {
+//             cout<<row<<" ";
+//             col = col+1;
+//         }
+//         cout<<endl;
+//         row= row +1;
+
+//     }
+
+// }
 
 // -----------------------Pattren-----------------------------------
 
+// #include <iostream>
+// using namespace std;
 
-#include <iostream>
-using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter the number of lines:";
+//     cin >> n;
 
 
-int main(){
-    int n ;
-    cin>>n;
+//     int num = 1;
+//     int row = 1;
 
-    int i = 1;
-    while (i<=n)
-    {
-        int j = 1;
-        while (j<=i)
-        {
-            cout<<i<<" ";
-            j = j+1;
-        }
-        cout<<endl;
-        i= i +1;
-        
-    }
-    
-}
+//     while (row <= n)
+//     {
+
+//         int col = 1;
+//         while (col <= row)
+//         {
+//             cout << num << " ";
+//             num = num +1;
+//             col = col + 1;
+//         }
+//         cout << endl;
+//         row = row + 1;
+
+//     }
+
+// }
+
+
 
