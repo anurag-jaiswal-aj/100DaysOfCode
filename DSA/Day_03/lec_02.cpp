4f4c8e2308f772cbe050418d14f112ac8a53f2b1
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-24 17:07:36 +0530

    .

--- DSA/Day_03/lec_02.cpp
+++ DSA/Day_03/lec_02.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+// int main()
+// {
+//     cout << "Hello world!" << endl;
 
-    int a = 123;
-    cout << a << endl;
-
-
-    char c = 'v' ; 
-    cout << c << endl;
+//     int a = 123;
+//     cout << a << endl;
 
 
-    bool bl = false ;
-    cout << bl << endl;
+//     char c = 'v' ; 
+//     cout << c << endl;
 
+
+//     bool bl = false ;
+//     cout << bl << endl;
+
+// }
+
+
+
+int main(){
+    int n;
+    cin >> n;
+
+
+    int i = 2;
+
+    while (i<n){
+        if (n%i ==0)
+        {
+            cout<< "Not Prime for "<< i <<endl;
+        }
+        else{
+            cout<< "Prime for "<< i <<endl;
+        }
+        i = i+1;
+    }
+    
 }
+
+
+
