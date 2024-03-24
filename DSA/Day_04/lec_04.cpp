335a176ae47b83b067eee531bf7bc1c28795562e
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-24 18:07:50 +0530

    .

--- DSA/Day_04/lec_04.cpp
+++ DSA/Day_04/lec_04.cpp
@@ -8,7 +8,7 @@
 //     int i= 1;
 
 //     while (i<=n){
-        
+
 //         int j = 1;
 //         while(j<=n){
 //             cout<< j;
@@ -18,34 +18,73 @@
 //         cout<<endl;
 //         i = i+1;
 //     }
-    
+
 // }
+
+// ----------------------------------------------------------
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int n ;
+//     cin>> n;
+
+//     int i= n;
+
+//     while (i>=1){
+
+//         int j = n;
+//         while(j>=1){
+//             cout<< j;
+//             j = j - 1;
+//         }
+
+//         cout<<endl;
+//         i = i - 1;
+//     }
+
+// }
+
 
 
 
 // ----------------------------------------------------------
 
+// #include <iostream>
+// using namespace std;
 
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     int i = 1;
+//     int count = 1;
+
+//     while (i <= n)
+//     {
+
+//         int j = 1;
+//         while (j <= n)
+//         {
+//             cout<<count<<"  ";
+//             count = count + 1;
+//             j = j + 1;
+//         }
+
+//         cout << endl;
+//         i = i + 1;
+//     }
+// }
+
+
+// ----------------------------------------------------------
 
 #include <iostream>
 using namespace std;
 
 int main(){
-    int n ;
-    cin>> n;
-
-    int i= n;
-
-    while (i>=1){
-        
-        int j = n;
-        while(j>=1){
-            cout<< j;
-            j = j - 1;
-        }
-
-        cout<<endl;
-        i = i - 1;
-    }
     
 }
+
