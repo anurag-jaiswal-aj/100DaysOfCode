a18e6f83ef63a2c0ccda1b117f5d9b9556c294ca
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-24 23:24:48 +0530

    .

--- DSA/Day_04/lec_04.cpp
+++ DSA/Day_04/lec_04.cpp
@@ -159,16 +159,12 @@
 
 #include <iostream>
 using namespace std;
-
 int main()
 {
-
     int n;
     cout << "Enter the value of n:";
     cin >> n;
-
     int i = 1;
-
     while (i <= n)
     {
         int j = 1;
