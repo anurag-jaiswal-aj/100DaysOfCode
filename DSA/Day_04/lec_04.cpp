4773ae7ee47a974783d170c8a40536b62107411f
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-26 23:56:07 +0530

    .

--- DSA/Day_04/lec_04.cpp
+++ DSA/Day_04/lec_04.cpp
@@ -364,6 +364,63 @@
 
 // --------------------------Pattern----------------------------------
 
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int n ;
+//     cin>>n;
+
+//     int i = 1;
+
+//     while(i<=n){
+//         int j = 1;
+//         char value = 'A' + n - i;
+//         while (j<=i)
+//         {
+//             cout<<value;
+//             value = value + 1;
+//             j = j + 1;
+//         }
+//         cout<<endl;
+//         i = i + 1;
+//     }
+// }
+
+
+
+// -----------------Pattern--------------------
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int n ;
+//     cin>>n;
+
+//     int i = 1;
+//     while(i<=n){
+//         int space = n - i;
+//         while(space){
+//             cout<<" ";
+//             space = space - 1;
+//         }
+//         int j = 1;
+//         while(j<=i){
+//             cout<<"*";
+//             j = j +1 ;
+//         }
+//         cout<<endl;
+//         i = i + 1;
+//     }
+// }
+
+
+
+
+// -----------------Pattern--------------------
+
+
 #include <iostream>
 using namespace std;
 
@@ -374,15 +431,24 @@ int main(){
     int i = 1;
 
     while(i<=n){
+        int space = n -i;
+        while(space){
+            cout<<" ";
+            space = space - 1;
+        }
         int j = 1;
-        char value = 'A' + n - i;
-        while (j<=i)
-        {
-            cout<<value;
-            value = value + 1;
-            j = j + 1;
+        while(j<=i){
+            cout<<j;
+            j = j +1;
+        }
+
+        int start = i - 1;
+        while(start){
+            cout<<start;
+            start = start - 1;
         }
         cout<<endl;
-        i = i + 1;
+        i = i +1;
     }
+
 }
