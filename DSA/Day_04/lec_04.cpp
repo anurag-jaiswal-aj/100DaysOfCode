315cceb8edb095baf1e42bf3ab28d9ee87cc5a25
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-26 23:06:20 +0530

    .

--- DSA/Day_04/lec_04.cpp
+++ DSA/Day_04/lec_04.cpp
@@ -284,6 +284,31 @@
 
 // --------------------Pattern-------------------------------
 
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+
+//     int i = 1;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         while (j <= n)
+//         {
+//             char ch = 'A' + i + j - 2;
+//             cout << ch;
+//             j = j + 1;
+//         }
+//         cout<<endl;
+//         i = i + 1;
+//     }
+// }
+
+
 #include <iostream>
 using namespace std;
 
@@ -294,16 +319,7 @@ int main()
 
 
     int i = 1;
-    while (i <= n)
-    {
-        int j = 1;
-        while (j <= n)
-        {
-            char ch = 'A' + i + j - 2;
-            cout << ch;
-            j = j + 1;
-        }
-        cout<<endl;
-        i = i + 1;
-    }
+
+
+
 }
