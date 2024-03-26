6ab7313494a3c6cdc6c85add6d4fe38f256b4a2b
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-26 19:27:21 +0530

    .

--- DSA/Day_04/lec_04.cpp
+++ DSA/Day_04/lec_04.cpp
@@ -230,27 +230,80 @@
 
 // --------------------Pattern-------------------------------
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main()
-{
+// int main()
+// {
 
-    int n;
-    cin >> n;
+//     int n;
+//     cin >> n;
 
-    int i = 1;
+//     int i = 1;
 
-    while (i <= n)
-    {
-        int j = 1;
-        while (j <= n)
-        {
-            char ch = 'A' + j - 1;
-            cout << ch;
-            j = j + 1;
-        }
-        cout << endl;
-        i = i + 1;
-    }
-}
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         while (j <= n)
+//         {
+//             char ch = 'A' + j - 1;
+//             cout << ch;
+//             j = j + 1;
+//         }
+//         cout << endl;
+//         i = i + 1;
+//     }
+// }
+
+// --------------------Pattern-------------------------------
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     int i = 1;
+//     char value = 'A';
+
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         while (j <= n)
+//         {
+//             cout << value;
+//             value = value + 1;
+//             j = j + 1;
+//         }
+//         cout << endl;
+//         i = i + 1;
+//     }
+// }
+
+// --------------------Pattern-------------------------------
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+
+//     int i = 1;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         while (j <= n)
+//         {
+//             char ch = 'A' + i + j - 2;
+//             cout << ch;
+//             j = j + 1;
+//         }
+//         cout<<endl;
+//         i = i + 1;
+//     }
+// }
