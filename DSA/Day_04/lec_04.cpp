2128f1648dc2bf37499ed6a2b2bd9711618029b1
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-26 23:10:08 +0530

    .

--- DSA/Day_04/lec_04.cpp
+++ DSA/Day_04/lec_04.cpp
@@ -317,9 +317,15 @@ int main()
     int n;
     cin >> n;
 
-
     int i = 1;
-
-
-
+    while(i<=n){
+        int j = 1;
+        while(j<=i){
+            char ch = 'A'+i-1;
+            cout<<ch;
+            j = j + 1;
+        }
+    cout<<endl;
+    i = i + 1;
+    }
 }
