7f70927554258a99ab1b09ce4b5670f5a56b6d59
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-26 19:15:41 +0530

    .

--- DSA/Day_04/lec_04.cpp
+++ DSA/Day_04/lec_04.cpp
@@ -157,23 +157,97 @@
 
 // -----------------------Pattren-----------------------------------
 
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cout << "Enter the value of n:";
+//     cin >> n;
+//     int i = 1;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         // int num = i;
+//         while (j <= i)
+//         {
+//             cout << (i + j - 1) << " ";
+//             // cout << num << " ";
+//             // num = num + 1;
+//             j = j + 1;
+//         }
+//         cout << endl;
+//         i = i + 1;
+//     }
+// }
+
+// --------------------Pattern-------------------------------
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     int i =1;
+//     while(i<=n){
+//         int j =1;
+//         while(j<=i){
+//             cout<<(i-j+1);
+//             j = j+1;
+//         }
+
+//         cout<<endl;
+//         i = i+1;
+//     }
+// }
+
+// --------------------Pattern-------------------------------
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     int i =1;
+//     while(i<=n){
+
+//         int j = 1;
+//         while (j<=n)
+//         {
+//             char ch = 'A'+i-1;
+//             cout<< ch;
+//             j = j+1;
+//         }
+//         cout<<endl;
+//         i = i+ 1;
+//     }
+
+// }
+
+// --------------------Pattern-------------------------------
+
 #include <iostream>
 using namespace std;
+
 int main()
 {
+
     int n;
-    cout << "Enter the value of n:";
     cin >> n;
+
     int i = 1;
+
     while (i <= n)
     {
         int j = 1;
-        // int num = i;
-        while (j <= i)
+        while (j <= n)
         {
-            cout << (i + j - 1) << " ";
-            // cout << num << " ";
-            // num = num + 1;
+            char ch = 'A' + j - 1;
+            cout << ch;
             j = j + 1;
         }
         cout << endl;
