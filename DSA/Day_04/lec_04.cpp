7052a17792c416b5e3e60a751d91eca79c98e2e8
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-26 23:24:24 +0530

    .

--- DSA/Day_04/lec_04.cpp
+++ DSA/Day_04/lec_04.cpp
@@ -309,23 +309,80 @@
 // }
 
 
+// --------------------------Pattern----------------------------------
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     int i = 1;
+//     while(i<=n){
+//         int j = 1;
+//         while(j<=i){
+//             char ch = 'A'+i-1;
+//             cout<<ch;
+//             j = j + 1;
+//         }
+//     cout<<endl;
+//     i = i + 1;
+//     }
+// }
+
+
+
+// --------------------------Pattern----------------------------------
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     int i = 1;
+//     char value = 'A';
+
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         while (j <= i)
+//         {
+//             cout << value;
+//             value = value + 1;
+//             j = j + 1;
+//         }
+//         cout << endl;
+//         i = i + 1;
+//     }
+// }
+
+
+// --------------------------Pattern----------------------------------
+
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
+int main(){
+    int n ;
+    cin>>n;
 
     int i = 1;
+
     while(i<=n){
         int j = 1;
-        while(j<=i){
-            char ch = 'A'+i-1;
-            cout<<ch;
+        char value = 'A' + n - i;
+        while (j<=i)
+        {
+            cout<<value;
+            value = value + 1;
             j = j + 1;
         }
-    cout<<endl;
-    i = i + 1;
+        cout<<endl;
+        i = i + 1;
     }
 }
