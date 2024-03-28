059f76cd87c7a125e28e32c6640dc7fc62cccc99
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-28 19:29:20 +0530

    .

---
+++ DSA/Day_05/lec_05.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 4;
+    int b = 6;
+
+    cout<<"A&B "<<(a&b)<<endl;
+    cout<<"A|B "<<(a|b)<<endl;
+    cout<<"~A "<<(~a)<<endl;
+    cout<<"A^B "<<(a^b)<<endl;
+}
