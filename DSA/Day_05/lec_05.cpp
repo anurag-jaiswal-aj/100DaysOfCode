8092a3b18d2bcaec9da87cb0b03ac40f87128ca8
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-28 20:03:59 +0530

    .

--- DSA/Day_05/lec_05.cpp
+++ DSA/Day_05/lec_05.cpp
@@ -1,13 +1,84 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//     int a = 4;
+//     int b = 6;
+
+// // ----------Bitwise operator-------------
+
+//     cout<<"A&B "<<(a&b)<<endl;
+//     cout<<"A|B "<<(a|b)<<endl;
+//     cout<<"~A "<<(~a)<<endl;
+//     cout<<"A^B "<<(a^b)<<endl;
+
+// // ----LEFT AND RIGHT SHIFT-----------
+
+//     cout<<(17>>1)<<endl;
+//     cout<<(17>>2)<<endl;
+//     cout<<(19<<1)<<endl;
+//     cout<<(21<<2)<<endl;
+
+// // ----------------------Pre and post Increment /Decrement--------------------------
+
+// int i = 5;
+
+// cout<< (++i)<<endl;
+// // 6
+// cout<< (i++)<<endl;
+// //6 , i = 7
+// cout<< (i--)<<endl;
+// //7 , i = 6
+// cout<< (--i)<<endl;
+// //5 , i =5
+// }
+
+// -----------------Questions output--------------
+
+// 1) 1
+// 2) stage 1 -Insider if  a = 0  and b = 3
+// 3) stage 1 -Insider if a =0 and b = 3
+// 4) 100
+// 5) b = 1 and c = 3
+
+//  -----------------For Loop---------------------
+
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+    // int n;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
 
-    int a = 4;
-    int b = 6;
+    // cout << "Printing Count from 1 to n:" << endl;
 
-    cout<<"A&B "<<(a&b)<<endl;
-    cout<<"A|B "<<(a|b)<<endl;
-    cout<<"~A "<<(~a)<<endl;
-    cout<<"A^B "<<(a^b)<<endl;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     cout << i << endl;
+    // }
+
+// --------------------------------------------------
+
+    // int n;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+
+    // cout << "Printing Count from 1 to n:" << endl;
+    // int i = 1;
+    // for (;;)
+    // {
+    //     if (i <= n)
+    //     {
+    //         cout << i << endl;
+    //     }
+    //     else
+    //     {
+    //         break;
+    //     }
+    //     i++;
+    // }
+
 }
