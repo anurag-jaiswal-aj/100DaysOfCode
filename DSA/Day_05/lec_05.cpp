51695597c75baf40c33a75ccde38b22f09d03d1c
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-28 23:55:52 +0530

    .

--- DSA/Day_05/lec_05.cpp
+++ DSA/Day_05/lec_05.cpp
@@ -100,3 +100,17 @@ int main()
     int n;
     cout << "Enter the value of n:" << endl;
     cin >> n;
+
+    int a = 0;
+    int b = 1;
+    cout<<a<<" "<<b<<" ";
+
+    for (int i = 0; i <= n; i++)
+    {
+        int nextNumber = a + b;
+        cout << nextNumber << " ";
+
+        a = b;
+        b = nextNumber;
+    }
+}
