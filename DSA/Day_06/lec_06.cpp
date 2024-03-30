5f72c0e34700e2b6202070d540820bbd422d2e8c
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-30 17:05:37 +0530

    .

--- DSA/Day_06/lec_06.cpp
+++ DSA/Day_06/lec_06.cpp
@@ -22,11 +22,27 @@ int main(){
     // cout<<"Answer is: "<<ans<<endl;
 
 
-// -------------------------------------------------------
+// -------------------binary to decimal------------------------------------
 
 
+// int n ;
+// cout<<"Enter the value of n: ";
+// cin>> n ;
 
+// int i = 0;
+// int ans = 0;
 
+// while(n != 0){
+
+//     int digit = n%10;
+
+//     if(digit == 1){
+//         ans = ans + pow(2 , i);
+//     }
+//     n = n/10;
+//     i++;
+// }
+// cout<<"Answer"<<ans<<endl;
 
 
 
