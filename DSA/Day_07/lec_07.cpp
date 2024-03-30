055f5e6a806035f49dc8520d634c45510b5e257b
Anurag Jaiswal <janurag582004@gmail.com>
2024-03-30 19:14:31 +0530

    .

---
+++ DSA/Day_07/lec_07.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+
+    // ---------------------------Reverse Integer----------------------
+
+    // int n ;
+    // cout<<"Enter the value of n: ";
+    // cin>> n ;
+
+    // int ans = 0;
+    //  while(n!=0){
+    //         int digit = n%10;
+    //         if((ans>INT32_MAX/10)|| (ans<INT32_MIN/10)){
+    //             return 0;
+    //         }
+    //         ans = (ans*10) + digit;
+    //         n = n / 10;
+    //     }
+    //     cout<<ans;
+
+    // --------------------Complement of base 10 integer----------------------------------------
+
+    // int n;
+    // cout<<"Enter the value of n: ";
+    // cin>>n;
+    // int m = n;
+    // int mask = 0;
+
+    // // edge case:
+    // if (n == 0)
+    // {
+    //     cout<<1;
+    // }
+
+    // while (m != 0)
+    // {
+    //     mask = (mask << 1) | 1;
+    //     m = m >> 1;
+    // }
+
+    // int ans = (~n) & mask;
+    // cout<<ans;
+
+    // ---------------------Power of two-----------------------
+
+    // int n;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+
+    // for (int i = 0; i <= 30; i++)
+    // {
+    //     int ans = pow(2, i);
+
+    //     if (ans == n)
+    //     {
+    //         return true;
+    //     }
+    // }
+    // return false;
+
+
+
+
+
+
+
+
+
+}
---
+++ DSA/Day_07/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+ // if((ans>INT32_MAX/10)|| (INT32_MIN/10)){
+            //     return 0;
+            // }
