e6e2d00ee88e8085b334ae7840699d63c4c86df3
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-03 23:14:26 +0530

    .

--- DSA/Day_12/lec_11.cpp
+++ DSA/Day_12/lec_11.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+// -------------------------Time Complexity-------------------------
+
+// --it is the amount of time taken by  ana algorithm to run
+
+// --- as a function of leght 0f the input
+
+
+// ---Why??  for making better programa and comparision of algorithm
+
+// ---Big O notation(uppper bound)   theta notation(for average case complexity) and   omega notation(lower bound)
+
+/// -----constant time - O(1) 
+/// -----Linear time -O(n)
+/// -----   Logarithmic time  -0(log n)
+// ----- Quadratic time - O(n^2)
+/// -----cubic time - O(n^3)
+
+
+
+// time complexity
+// (top to bottom complexity decrases)
+
+/*   O(N!)  (highrst complexity)
+     O(2^n)
+     O(N^3)
+     O(N^2)
+     O(NlogN)
+     O(N)
+     O(log N)
+     O(1)    (lowest complexity)
 
 
 
 
-    return 0 ;
+*/
+
+
+
+
+
+int main()
+{
+
+    return 0;
 }
