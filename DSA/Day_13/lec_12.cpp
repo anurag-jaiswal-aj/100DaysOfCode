34862b7c786eb603e679b76795bbc5761b8c15f1
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-04 20:01:04 +0530

    .

--- DSA/Day_13/lec_12.cpp
+++ DSA/Day_13/lec_12.cpp
@@ -36,11 +36,11 @@
 //     int even[6] = {2, 4, 6, 8, 12, 18};
 //     int odd[5] = {3, 8, 11, 14, 16};
 
-//     int indexeven = binarySearch(even, 6, 12);
-//     int indexodd = binarySearch(odd, 5, 11);
+//     int evenindex = binarySearch(even, 6, 12);
+//     int oddindex = binarySearch(odd, 5, 11);
 
-//     cout << "Index of 12 is:" << indexeven << endl;
-//     cout << "Index of 11 is:" << indexodd << endl;
+//     cout << "Index of 12 is:" << evenindex << endl;
+//     cout << "Index of 11 is:" << oddindex << endl;
 
 //     return 0;
 // }
