c18a1042c1ca0a2a6e4c36876ce5591facbcb0f9
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-04 23:12:12 +0530

    .

--- DSA/Day_13/lec_12.cpp
+++ DSA/Day_13/lec_12.cpp
@@ -1,55 +1,54 @@
 
 // --------------------------Binary Search-------------------------
 
+#include <iostream>
+using namespace std;
 
-// #include <iostream>
-// using namespace std;
+int binarySearch(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = (start + end) / 2;
+    // int mid = start + (end - start) / 2; //Optimisation (For value of integer outof range)
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
 
-// int binarySearch(int arr[], int size, int key)
-// {
-//     int start = 0;
-//     int end = size - 1;
-//     int mid = (start + end) / 2;
-//     while (start <= end)
-//     {
-//         if (arr[mid] == key)
-//         {
-//             return mid;
-//         }
+        if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
 
-//         if (key > arr[mid])
-//         {
-//             start = mid + 1;
-//         }
-//         else
-//         {
-//             end = mid - 1;
-//         }
+        mid = (start + end) / 2;
+        // mid = start + (end - start) / 2;   //Optimisation
+    }
+    return -1;
+}
 
-//         mid = (start + end) / 2;
-//     }
-//     return -1;
-// }
+int main()
+{
+    int even[6] = {2, 4, 6, 8, 12, 18};
+    int odd[5] = {3, 8, 11, 14, 16};
 
-// int main()
-// {
-//     int even[6] = {2, 4, 6, 8, 12, 18};
-//     int odd[5] = {3, 8, 11, 14, 16};
+    int evenindex = binarySearch(even, 6, 12);
+    int oddindex = binarySearch(odd, 5, 11);
 
-//     int evenindex = binarySearch(even, 6, 12);
-//     int oddindex = binarySearch(odd, 5, 11);
+    cout << "Index of 12 is:" << evenindex << endl;
+    cout << "Index of 11 is:" << oddindex << endl;
 
-//     cout << "Index of 12 is:" << evenindex << endl;
-//     cout << "Index of 11 is:" << oddindex << endl;
-
-//     return 0;
-// }
+    return 0;
+}
 
 // /*O/P:
 // Index of 12 is : 4
 // Index of 12 is : 2
 // */
-
-
 
 //----------------------------------------------------------------
