c774c18b054f36bc70192c954a02353f2cd55ff0
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-04 19:58:15 +0530

    .

--- DSA/Day_13/lec_12.cpp
+++ DSA/Day_13/lec_12.cpp
@@ -1,15 +1,55 @@
-#include <iostream>
-using namespace std;
+
+// --------------------------Binary Search-------------------------
 
 
-int main(){
-    
+// #include <iostream>
+// using namespace std;
+
+// int binarySearch(int arr[], int size, int key)
+// {
+//     int start = 0;
+//     int end = size - 1;
+//     int mid = (start + end) / 2;
+//     while (start <= end)
+//     {
+//         if (arr[mid] == key)
+//         {
+//             return mid;
+//         }
+
+//         if (key > arr[mid])
+//         {
+//             start = mid + 1;
+//         }
+//         else
+//         {
+//             end = mid - 1;
+//         }
+
+//         mid = (start + end) / 2;
+//     }
+//     return -1;
+// }
+
+// int main()
+// {
+//     int even[6] = {2, 4, 6, 8, 12, 18};
+//     int odd[5] = {3, 8, 11, 14, 16};
+
+//     int indexeven = binarySearch(even, 6, 12);
+//     int indexodd = binarySearch(odd, 5, 11);
+
+//     cout << "Index of 12 is:" << indexeven << endl;
+//     cout << "Index of 11 is:" << indexodd << endl;
+
+//     return 0;
+// }
+
+// /*O/P:
+// Index of 12 is : 4
+// Index of 12 is : 2
+// */
 
 
 
-
-
-
-
-    return 0;
-}
+//----------------------------------------------------------------
