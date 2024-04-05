da6526ad7b57458d62a1bed7ce4bc7dd723fdb39
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-05 20:44:10 +0530

    .

--- DSA/Day_14/lec_13.cpp
+++ DSA/Day_14/lec_13.cpp
@@ -19,7 +19,7 @@ int firstOcc(int arr[], int n, int key)
         }
         else if (key > arr[mid])
         {
-            e = mid + 1;
+            s = mid + 1;
         }
 
         else
@@ -30,16 +30,54 @@ int firstOcc(int arr[], int n, int key)
         mid = s + (e - s) / 2;
     }
     return ans;
-    cout<<ans;
 }
+
+
+
+
+int lastOcc(int arr[], int n, int key)
+{
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+
+    while (s <= e)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if (key > arr[mid])
+        {
+            s = mid + 1;
+        }
+
+        else
+        {
+            e = mid - 1;
+        }
+
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
+
+
 
 int main()
 {
     int even[6] = {1, 2, 3, 3, 5, 6};
-    int odd[7] = {1, 2, 3, 6, 2, 2, 2};
-
-    cout << "First Occurence of 3 is at index:" << firstOcc(even, 6, 3);
-    cout << "First Occurence of 2 is at index:" << firstOcc(odd, 7, 2);
+    
+    cout << "First Occurence of 3 is at index:" << firstOcc(even, 6, 3)<<endl;
+    cout << "First Occurence of 3 is at index:" << lastOcc(even, 6, 3)<<endl;
 
     return 0;
 }
+
+
+
+
+
+// ------------------------------------------------------------------------------------
