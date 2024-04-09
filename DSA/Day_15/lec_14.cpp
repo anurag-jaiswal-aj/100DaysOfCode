8c31cb897ef8ffedb119e1197322dace1d86a917
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-09 16:07:03 +0530

    .

--- DSA/Day_15/lec_14.cpp
+++ DSA/Day_15/lec_14.cpp
@@ -1,8 +1,50 @@
-#include <iostream>
+
+// -----------------------------Pivot in array-----------------------------------------------------------------------
+
+
+// #include <iostream>
+// using namespace std;
+
+// int getPivot(int arr[], int n)
+// {
+//     int s = 0;
+//     int e = n - 1;
+//     int mid = s + (e - s) / 2;
+
+//     while (s < e)
+//     {
+//         if (arr[mid] >= arr[0])
+//         {
+//             s = mid + 1;
+//         }
+//         else
+//         {
+//             e = mid;
+//         }
+//         mid = s + (e - s) / 2;
+//     }
+//     return s;
+// }
+
+// int main()
+// {
+
+//     int arr[5] = {8, 10, 17, 1, 3};
+//     cout << "Pivot is" << getPivot(arr, 5) << endl;
+//     return 0;
+// }
+
+
+
+
+
+
+// ----------------------Search in rotated and sorted array-----------------------------------------
+
+#include<iostream>
 using namespace std;
 
 int main(){
-
 
 
     return 0;
