485563a0d163d76f75ff83b98b0c45a20336f169
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-13 23:00:50 +0530

    .

--- DSA/Day_28/lec_17.cpp
+++ DSA/Day_28/lec_17.cpp
@@ -22,12 +22,17 @@ int main()
     // Bubble Sorting algorithm
     for (int i = 1; i < n; i++)
     {
+        bool swapped = false;  ///for optimisation of code
         for (int j = 0; j < n - i; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
+        }
+        if(swapped == false){
+            break;
         }
     }
 
