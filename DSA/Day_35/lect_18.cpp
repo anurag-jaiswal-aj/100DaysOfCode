0f66e54d58150200e4a5df8b63b4bfcbc8c0503b
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-14 10:12:11 +0530

    .

--- DSA/Day_35/lect_18.cpp
+++ DSA/Day_35/lect_18.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    cout << "Enter the array elements: ";
+    int arr[n];
 
+    // Taking array elements input
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
+    // Insertion Sorting algorithm
+    for (int i = 1; i < n; i++) {
+        int temp = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+
+    // Printing the output
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
