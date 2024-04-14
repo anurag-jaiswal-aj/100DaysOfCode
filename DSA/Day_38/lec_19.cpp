7d9971aeca2c622eea474e7d58652e47ba34f0f9
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-14 10:55:16 +0530

    .

--- DSA/Day_38/lec_19.cpp
+++ DSA/Day_38/lec_19.cpp
@@ -30,25 +30,81 @@
 
 // ---------------------------------STL Vector--------------------------------
 
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main()
+// {
+//     vector<int> v;
+
+//     vector<int> a(5, 1);
+//     cout << "Print a: " << endl;
+//     for (int i : a)
+//     {
+//         cout << i << " ";
+//     }
+//     cout << endl;
+
+//     vector<int>last(a);
+//     cout << "Print last: " << endl;
+//     for (int i : last)
+//     {
+//         cout << i << " ";
+//     }
+//     cout << endl;
+
+//     cout << "Size of the vector:" << v.capacity() << endl;
+
+//     v.push_back(1);
+//     cout << "Size of the vector:" << v.capacity() << endl;
+
+//     v.push_back(2);
+//     cout << "Size of the vector:" << v.capacity() << endl;
+
+//     v.push_back(3);
+//     cout << "Size of the vector(no of element + vacant space):" << v.capacity() << endl;
+//     cout << "Size of vector(no of element)" << v.size() << endl;
+
+//     cout << "Element at indx 2 is: " << v.at(2) << endl;
+
+//     cout << "First Element: " << v.front() << endl;
+//     cout << "Last Element: " << v.back() << endl;
+
+//     cout << "Before Pop:" << endl;
+//     for (int i : v)
+//     {
+//         cout << i << " ";
+//     }
+//     cout << endl;
+
+//     v.pop_back();
+
+//     cout << "After Pop" << endl;
+
+//     for (int i : v)
+//     {
+//         cout << i << " ";
+//     }
+
+//     cout << "Size Before clear: " << v.size() << endl;
+//     cout << "Capacity Before clear: " << v.capacity() << endl;
+
+//     v.clear();
+
+//     cout << "Size After clear: " << v.size() << endl;
+//     cout << "Capacity After clear: " << v.capacity() << endl;
+
+//     return 0;
+// }
+
+// --------------------------------Deque----------------------------
+
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main()
 {
-    vector<int> v;
-    cout << "Size of the vector:" << v.capacity() << endl;
 
-    v.push_back(1);
-    cout << "Size of the vector:" << v.capacity() << endl;
-
-    v.push_back(2);
-    cout << "Size of the vector:" << v.capacity() << endl;
-
-    v.push_back(3);
-    cout << "Size of the vector(no of element + vacant space):" << v.capacity() << endl;
-    cout << "Size of vector(no of element)" << v.size() << endl;
-
-    cout << "Element at indx 2 is: " << v.at(2) << endl;
     return 0;
 }
