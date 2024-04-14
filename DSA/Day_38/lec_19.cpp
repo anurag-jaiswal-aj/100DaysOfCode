868598931d0c0d95cad4ab0bb4b3d5c059523230
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-14 12:13:18 +0530

    .

--- DSA/Day_38/lec_19.cpp
+++ DSA/Day_38/lec_19.cpp
@@ -100,11 +100,85 @@
 
 // --------------------------------Deque----------------------------
 
+// #include <iostream>
+// #include <deque>
+// using namespace std;
+
+// int main()
+// {
+//     deque<int> d;
+
+//     d.push_back(1);
+//     d.push_front(2);
+//     // for (int i : d)
+//     // {
+//     //     cout << i << " ";
+//     // }
+
+//     // d.pop_back();
+//     // cout << endl;
+//     // for (int i : d)
+//     // {
+//     //     cout << i << " ";
+//     // }
+
+//     cout << endl;
+
+//     cout << "Print first index element:  " << d.at(1) << endl;
+
+//     cout<<"Front: "<<d.front()<<endl;
+//     cout<<"Back: "<<d.back()<<endl;
+
+//     cout<<"Empty or not: "<<d.empty()<<endl;
+
+//     cout<<"Before Erase: "<<d.size()<<endl;
+//     // d.erase();
+//     d.erase(d.begin(),d.begin()+1);
+//     cout<<"After Erase: "<<d.size()<<endl;
+//     for(int i:d){
+//         cout<<i<<endl;
+//     }
+
+//     return 0;
+// }
+
+// ---------------------------List-------------------------------
+
 #include <iostream>
+#include <list>
 using namespace std;
 
 int main()
 {
+    list<int> l;
+    list<int> n(5,100);
+    
+
+    cout<<"Printing n: "<<endl;
+    for(int i:n){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+
+    l.push_back(1);
+    l.push_front(2);
+
+    for(int i:l){
+        cout<<i<<" ";
+    }
+
+
+    cout<<endl;
+    l.erase(l.begin());
+    cout<<"After Erase"<<endl;
+    for(int i:l){
+        cout<<i<<" ";
+    }
+
+    cout<<"Size of List is: "<<l.size()<<endl;
+
+
 
     return 0;
 }
