43f1f711f26cd291aef51f12113e81b24c71495f
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-15 17:13:18 +0530

    .

--- DSA/Day_38/lec_19.cpp
+++ DSA/Day_38/lec_19.cpp
@@ -346,40 +346,40 @@
 // -----------------------STL Algorithm-----------------------------
 
 
-#include <iostream>
-#include <algorithm>
-#include <vector>
+// #include <iostream>
+// #include <algorithm>
+// #include <vector>
 
-using namespace std;
-int main(){
-    vector<int> v;
-    v.push_back(1);
-    v.push_back(3);
-    v.push_back(6);
-    v.push_back(7);
+// using namespace std;
+// int main(){
+//     vector<int> v;
+//     v.push_back(1);
+//     v.push_back(3);
+//     v.push_back(6);
+//     v.push_back(7);
 
-    cout<<"Finding 6: "<<binary_search(v.begin(),v.end(),6)<<endl;
-
-
-
-    int a = 4;
-    int b = 5;
-
-    cout<<"Max: "<<max(a,b)<<endl;
-
-    cout<<"Min: "<<min(a,b)<<endl;
-
-    swap(a,b);
-
-    cout<<"A: "<<a<<endl;
-    cout<<"B: "<<b<<endl;
+//     cout<<"Finding 6: "<<binary_search(v.begin(),v.end(),6)<<endl;
 
 
 
-    string abcd ="abcd";
-    reverse(abcd.begin(),abcd.end());
-    cout<<"String: "<<abcd<<endl;
+//     int a = 4;
+//     int b = 5;
+
+//     cout<<"Max: "<<max(a,b)<<endl;
+
+//     cout<<"Min: "<<min(a,b)<<endl;
+
+//     swap(a,b);
+
+//     cout<<"A: "<<a<<endl;
+//     cout<<"B: "<<b<<endl;
 
 
 
-}
+//     string abcd ="abcd";
+//     reverse(abcd.begin(),abcd.end());
+//     cout<<"String: "<<abcd<<endl;
+
+
+
+// }
