a1ae0e9cf6f492e08fa9d960df4bcde1e8788597
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-26 00:30:52 +0530

    .

--- DSA/Day_39/lec_20.cpp
+++ DSA/Day_39/lec_20.cpp
@@ -122,12 +122,27 @@
 
 
 // -------------------------------move zeroes---------------
-
 #include <iostream>
+#include <vector> 
 using namespace std;
 
-int main(){
+int main() {
+    
+    vector<int> nums = {0, 1, 0, 3, 12};
+
+    int i = 0;
+    for (int j = 0; j < nums.size(); j++) {
+        if (nums[j] != 0) { 
+            swap(nums[j], nums[i]);
+            i++;
+        }
+    }
 
     
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
     return 0;
 }
