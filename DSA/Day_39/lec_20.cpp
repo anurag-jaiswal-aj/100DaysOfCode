57945cae0859ca2b165ccab9a4dc94020e0daa55
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-25 19:56:49 +0530

    .

--- DSA/Day_39/lec_20.cpp
+++ DSA/Day_39/lec_20.cpp
@@ -1,52 +1,82 @@
 
 // -----------------------------------reverse an array----------------------
 
-#include <iostream>
-#include <vector>
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// vector<int> reverse(vector<int> arr)
+// {
+
+//     int s = 0, e = arr.size() - 1;
+
+//     while (s <= e)
+//     {
+//         swap(arr[s], arr[e]);
+//         s++;
+//         e--;
+//     }
+//     return arr;
+// }
+
+// void print(vector<int> arr)
+// {
+
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
+
+// int main()
+// {
+
+//     vector<int> arr;
+
+//     arr.push_back(11);
+//     arr.push_back(7);
+//     arr.push_back(3);
+//     arr.push_back(12);
+//     arr.push_back(4);
+
+//     vector<int> ans = reverse(arr);
+
+//     cout << "Printing  array:" << endl;
+//     print(arr);
+
+//     cout << "Printing reverse array:" << endl;
+//     print(ans);
+
+//     return 0;
+// }
+
+
+
+
+// -------------------------Merge two Sorted Array-----------------------
+
+#include<iostream>
+#include<vector>
 using namespace std;
 
-vector<int> reverse(vector<int> arr)
-{
+void merge(int arr1[] , int n , int arr2[] , int m , int arr[]){
 
-    int s = 0, e = arr.size() - 1;
-
-    while (s <= e)
-    {
-        swap(arr[s], arr[e]);
-        s++;
-        e--;
-    }
-    return arr;
 }
 
-void print(vector<int> arr)
-{
+int main(){
+    int arr1[5] ={1,3,5,7,9};
+    int arr2[4] ={2,4,6,8};
 
-    for (int i = 0; i < arr.size(); i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
+    int arr[0] = {0};
 
-int main()
-{
+    merge(arr1, 5 , arr2 , 4 , arr);
 
-    vector<int> arr;
+    print(arr ,0);
 
-    arr.push_back(11);
-    arr.push_back(7);
-    arr.push_back(3);
-    arr.push_back(12);
-    arr.push_back(4);
 
-    vector<int> ans = reverse(arr);
 
-    cout << "Printing  array:" << endl;
-    print(arr);
-
-    cout << "Printing reverse array:" << endl;
-    print(ans);
 
     return 0;
 }
+
