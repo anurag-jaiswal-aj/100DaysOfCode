83fc94c8551149d759c9baa33100925ca7e72c3d
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-25 23:58:39 +0530

    .

--- DSA/Day_39/lec_20.cpp
+++ DSA/Day_39/lec_20.cpp
@@ -54,29 +54,80 @@
 
 
 
+
 // -------------------------Merge two Sorted Array-----------------------
 
-#include<iostream>
-#include<vector>
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// void merge(int arr1[], int n, int arr2[], int m, int arr[])
+// {
+
+//     int i = 0, j = 0;
+//     int k = 0;
+//     while (i < n && j < m)
+//     {
+//         if (arr1[i] < arr2[j])
+//         {
+//             arr[k] = arr1[i];
+//             k++;
+//             i++;
+//         }
+//         else
+//         {
+//             arr[k] = arr2[j];
+//             k++;
+//             j++;
+//         }
+//     }
+
+//     while (i < n)
+//     {
+//         arr[k++] = arr1[i++];
+        
+//     }
+
+//     while (j < m)
+//     {
+//         arr[k++] = arr2[j++];
+        
+//     }
+// }
+
+
+// void print(int arr[], int n){
+//     for(int i =0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+// }
+
+// int main()
+// {
+//     int arr1[5] = {1, 3, 5, 7, 9};
+//     int arr2[4] = {2, 4, 6, 8};
+
+//     int arr[9] = {};
+
+//     merge(arr1, 5, arr2, 4, arr);
+
+//     print(arr, 9);
+
+//     return 0;
+// }
+
+
+
+
+// -------------------------------move zeroes---------------
+
+#include <iostream>
 using namespace std;
 
-void merge(int arr1[] , int n , int arr2[] , int m , int arr[]){
-
-}
-
 int main(){
-    int arr1[5] ={1,3,5,7,9};
-    int arr2[4] ={2,4,6,8};
 
-    int arr[0] = {0};
-
-    merge(arr1, 5 , arr2 , 4 , arr);
-
-    print(arr ,0);
-
-
-
-
+    
     return 0;
 }
-
