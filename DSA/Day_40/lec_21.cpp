aaa5dbe3f09e3bee33b45b4ede5a0558a52dab39
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-26 12:48:42 +0530

    .

--- DSA/Day_40/lec_21.cpp
+++ DSA/Day_40/lec_21.cpp
@@ -38,27 +38,77 @@
 //--------------check if array is sorted and rotated-----------------------------------------------------------
 
 
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// bool isCyclicallySorted(const vector<int>& nums) {
+//     int count = 0;
+//     int n = nums.size();
+//     for (int i = 0; i < n; i++) {
+//         if (nums[i] > nums[(i + 1) % n]) { // Compare with the next element in a cyclic manner
+//             count++;
+//         }
+//     }
+
+//     return count <= 1; // If count is less than or equal to 1, the array is cyclically sorted
+// }
+
+// int main() {
+//     vector<int> nums = {4, 5, 6, 7, 1, 2, 3}; 
+
+//     bool sorted = isCyclicallySorted(nums);
+//     cout << "Is the array cyclically sorted? " << (sorted ? "Yes" : "No") << endl;
+
+//     return 0;
+// }
+
+
+
+
+
+//-----------------------------------------Add two arrays---------------------------------------------------------
 #include <iostream>
 #include <vector>
+#include "../../../../../../MinGW/lib/gcc/mingw32/6.3.0/include/c++/bits/algorithmfwd.h"
 using namespace std;
 
-bool isCyclicallySorted(const vector<int>& nums) {
-    int count = 0;
-    int n = nums.size();
-    for (int i = 0; i < n; i++) {
-        if (nums[i] > nums[(i + 1) % n]) { // Compare with the next element in a cyclic manner
-            count++;
-        }
+vector<int> addTwoArrays(const vector<int>& a, const vector<int>& b) {
+    vector<int> ans;
+    int i = a.size() - 1;
+    int j = b.size() - 1;
+    int carry = 0;
+
+    while (i >= 0 || j >= 0) {
+        int val1 = (i >= 0) ? a[i] : 0;
+        int val2 = (j >= 0) ? b[j] : 0;
+        int sum = val1 + val2 + carry;
+        carry = sum / 10;
+        sum %= 10;
+        ans.push_back(sum);
+        i--;
+        j--;
     }
 
-    return count <= 1; // If count is less than or equal to 1, the array is cyclically sorted
+    if (carry != 0) {
+        ans.push_back(carry);
+    }
+
+    reverse(ans.begin(), ans.end()); // Reverse the vector to get the correct order
+    return ans;
 }
 
 int main() {
-    vector<int> nums = {4, 5, 6, 7, 1, 2, 3}; 
+    vector<int> a = {1, 2, 3};
+    vector<int> b = {4, 5, 6};
 
-    bool sorted = isCyclicallySorted(nums);
-    cout << "Is the array cyclically sorted? " << (sorted ? "Yes" : "No") << endl;
+    vector<int> result = addTwoArrays(a, b);
+
+    // Output the result array
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
