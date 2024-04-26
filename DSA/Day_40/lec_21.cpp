df50b6a3ff47964d3d0f01bb4771159fa83c0533
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-26 12:06:15 +0530

    .

--- DSA/Day_40/lec_21.cpp
+++ DSA/Day_40/lec_21.cpp
@@ -1,12 +1,32 @@
+
+
 // --------------------------Rotate Arrays------------------------------------------------------------
 
+
 #include <iostream>
+#include <vector> 
 using namespace std;
 
-int main(){
+int main()
+{
+    vector<int> nums = {1, 2, 3, 4, 5}; 
+    int k = 2;                          // Rotation amount
+
+    vector<int> temp(nums.size()); // temporary vector to hold rotated elements
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        temp[(i + k) % nums.size()] = nums[i]; // Rotate elements
+    }
+
+    nums = temp; // Update nums with rotated elements
 
     
-
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
