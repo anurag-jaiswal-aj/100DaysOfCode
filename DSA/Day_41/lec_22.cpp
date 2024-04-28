bead122947916f6c3aa575ec5fe8037a2666ba37
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-28 20:07:49 +0530

    .

--- DSA/Day_41/lec_22.cpp
+++ DSA/Day_41/lec_22.cpp
@@ -1,5 +1,5 @@
 
-
+/*
 #include <iostream>
 using namespace std;
 
@@ -112,5 +112,34 @@ int main()
     cin >> s;
     cout << getMaxOccCharacter(s) << endl;
 
+    return 0;
+}
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str ;
+    cin >> str;
+    string temp = "";
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == ' ')
+        {
+            temp.push_back('@');
+            temp.push_back('4');
+            temp.push_back('0');
+        }
+        else
+        {
+            temp.push_back(str[i]);
+        }
+    }
+
+    cout << "Modified string: " << temp << endl;
     return 0;
 }
