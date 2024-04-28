1ee7e85d5a8cdab2d974a8174d3cdd1cb3979111
Anurag Jaiswal <janurag582004@gmail.com>
2024-04-29 01:03:10 +0530

    .

--- DSA/Day_43/lec_23.cpp
+++ DSA/Day_43/lec_23.cpp
@@ -2,23 +2,57 @@
 
 // ----------------------creating input and output 2-D array--------------------------------------------------------
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main(){
+// int main()
+// {
 
+//     int arr[3][4];
 
+// int arr[3][4] = {1,2,4,5,6,7,8,9,10,11,12}
+//  int arr[3][4] = {{1,11,111,1111},{2,22,222,2222},{3,33,333,3333}}
 
+//     // taking input row wise
+//     cout << "Enter elements" << endl;
+//     for (int row = 0; row < 3; row++)
+//     {
+//         for (int col = 0; col < 4; col++)
+//         {
+//             cin >> arr[row][col];
+//         }
+//     }
 
+//     // taking iput coloumn wise
+//     cout << "Enter elements" << endl;
+//     for (int col = 0; col < 4; col++)
+//     {
+//         for (int row = 0; row < 3; row++)
+//         {
+//             cin >> arr[row][col];
+//         }
+//     }
 
+//     // printing
+//     for (int col = 0; col < 3; col++)
+//     {
+//         for (int row = 0; row < 4; row++)
+//         {
+//             cout << arr[row][col] << " ";
+//         }
 
+//         cout << endl;
+//     }
 
+//     for (int row = 0; row < 3; row++)
+//     {
+//         for (int col = 0; col < 4; col++)
+//         {
+//             cout << arr[col][row] << " ";
+//         }
 
+//         cout << endl;
+//     }
 
-
-
-
-
-
-    return 0;
-}
+//     return 0;
+// }
