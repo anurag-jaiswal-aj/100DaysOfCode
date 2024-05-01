8f91c05aba4ffcedfe2e731e73552e6eeb054bc9
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-02 00:26:37 +0530

    .

--- DSA/Day_44/lec_24.cpp
+++ DSA/Day_44/lec_24.cpp
@@ -1,9 +1,93 @@
+
+// ---------------Prime number(Brute force approach---------------------
+
+// #include <iostream>
+// using namespace std;
+
+// bool isPrime(int n){
+
+//     if(n<=1){
+//         return false;
+//     }
+
+//     for(int i = 2;i<n;i++){
+//         if(n%i == 0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+
+
+// int main(){
+
+//     int n;
+//     cout<<"Enter the value of n:"<<endl;
+//     cin>>n;
+
+//     if(isPrime(n)){
+//         cout<<"It is a prime number:!"<<endl;
+//     }
+//     else{
+//         cout<<"Not a prime number!"<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+
+// -------------------- implementation of the Sieve of Eratosthenes algorithm to count prime numbers-------------------------
+
+
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
+class Solution {
+public:
+    int countPrimes(int n) {
+        int cnt = 0;
+        std::vector<bool> prime(n + 1, true);
 
+        prime[1] = prime[0] = false;
 
+        for (int i = 2; i < n; i++) {
+            if (prime[i]) {
+                cnt++;
 
+                for (int j = 2 * i; j < n; j += i) { 
+                    prime[j] = 0;
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter the value of n:" << endl;
+    cin >> n;
+
+    Solution sol;
+    int primesCount = sol.countPrimes(n);
+
+    cout << "Number of prime numbers less than " << n << " is: " << primesCount << endl;
+
+    bool isPrime = true;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        cout << "It is a prime number!" << endl;
+    } else {
+        cout << "Not a prime number!" << endl;
+    }
     return 0;
 }
+
