1a767bb8d504803d3f733733f42d7db4f12c0b24
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-02 20:20:04 +0530

    .

--- DSA/Day_47/lec_25.cpp
+++ DSA/Day_47/lec_25.cpp
@@ -1,33 +1,92 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+
+//     int num = 5;
+
+//     cout << num << endl;
+
+//     // address of operator - &
+
+//     //pointers are used to store address
+
+//     cout << "Address of num is:" << &num << endl;
+
+//     int *ptr =&num;
+
+//     cout<<"Address is:"<<ptr<<endl;
+//     cout<<"Value is:"<<*ptr<<endl;
+
+//     double d = 4.3;
+//     double *p2 = &d;
+
+//     cout<<"Address is:"<<p2<<endl;
+//     cout<<"Value is:"<<*p2<<endl;
+
+//     cout<<"Size of integer is:"<<sizeof(num)<<endl;
+//     cout<<"Size of pointer is:"<<sizeof(ptr)<<endl;
+
+//     cout<<"Size of pointer is:"<<sizeof(p2)<<endl;
+//     return 0;
+// }
+
+
+
+// -------------------------------------
+
+
+
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
 
+    // pointer to int is created, and pointing to some garbage address
+    // int *p = 0;
+
+    // cout << *p << endl;
+
+    int i = 5;
+
+    int *q = &i;
+    cout << q << endl;
+    cout << *q << endl;
+
+    int *p = 0;
+    p = &i;
+
+    cout << p << endl;
+    cout << *p << endl;
+
     int num = 5;
+    int a = num;
+    cout << "a before " << num << endl;
+    a++;
+    cout << "a after " << num << endl;
 
-    cout << num << endl;
+    int *p = &num;
+    cout << "before " << num << endl;
+    (*p)++;
+    cout << "after " << num << endl;
 
-    // address of operator - &
+    // copying a pointer
+    int *q = p;
+    cout << p << " - " << q << endl;
+    cout << *p << " - " << *q << endl;
 
-    //pointers are used to store address
+    // important concept
+    int i = 3;
+    int *t = &i;
+    // cout <<  (*t)++ << endl;
+    *t = *t + 1;
+    cout << *t << endl;
+    cout << " before t " << t << endl;
+    t = t + 1;
+    cout << " after t " << t << endl;
 
-    cout << "Address of num is:" << &num << endl;
-
-    int *ptr =&num;
-
-    cout<<"Address is:"<<ptr<<endl;
-    cout<<"Value is:"<<*ptr<<endl;
-
-    double d = 4.3;
-    double *p2 = &d;
-
-    cout<<"Address is:"<<p2<<endl;
-    cout<<"Value is:"<<*p2<<endl;
-
-    cout<<"Size of integer is:"<<sizeof(num)<<endl;
-    cout<<"Size of pointer is:"<<sizeof(ptr)<<endl;
-
-    cout<<"Size of pointer is:"<<sizeof(p2)<<endl;
     return 0;
 }
