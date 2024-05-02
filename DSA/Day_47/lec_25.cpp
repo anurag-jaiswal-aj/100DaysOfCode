490980310e88745b89f726c343c65c7fd98e2b1e
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-02 20:20:19 +0530

    .

--- DSA/Day_47/lec_25.cpp
+++ DSA/Day_47/lec_25.cpp
@@ -50,43 +50,43 @@ int main()
 
     // cout << *p << endl;
 
-    int i = 5;
+    // int i = 5;
 
-    int *q = &i;
-    cout << q << endl;
-    cout << *q << endl;
+    // int *q = &i;
+    // cout << q << endl;
+    // cout << *q << endl;
 
-    int *p = 0;
-    p = &i;
+    // int *p = 0;
+    // p = &i;
 
-    cout << p << endl;
-    cout << *p << endl;
+    // cout << p << endl;
+    // cout << *p << endl;
 
-    int num = 5;
-    int a = num;
-    cout << "a before " << num << endl;
-    a++;
-    cout << "a after " << num << endl;
+    // int num = 5;
+    // int a = num;
+    // cout << "a before " << num << endl;
+    // a++;
+    // cout << "a after " << num << endl;
 
-    int *p = &num;
-    cout << "before " << num << endl;
-    (*p)++;
-    cout << "after " << num << endl;
+    // int *p = &num;
+    // cout << "before " << num << endl;
+    // (*p)++;
+    // cout << "after " << num << endl;
 
-    // copying a pointer
-    int *q = p;
-    cout << p << " - " << q << endl;
-    cout << *p << " - " << *q << endl;
+    // // copying a pointer
+    // int *q = p;
+    // cout << p << " - " << q << endl;
+    // cout << *p << " - " << *q << endl;
 
-    // important concept
-    int i = 3;
-    int *t = &i;
-    // cout <<  (*t)++ << endl;
-    *t = *t + 1;
-    cout << *t << endl;
-    cout << " before t " << t << endl;
-    t = t + 1;
-    cout << " after t " << t << endl;
+    // // important concept
+    // int i = 3;
+    // int *t = &i;
+    // // cout <<  (*t)++ << endl;
+    // *t = *t + 1;
+    // cout << *t << endl;
+    // cout << " before t " << t << endl;
+    // t = t + 1;
+    // cout << " after t " << t << endl;
 
     return 0;
 }
