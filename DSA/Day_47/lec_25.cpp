651e1c02e88d4501a78ab59572a6f755df2a9591
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-02 19:58:44 +0530

    .

--- DSA/Day_47/lec_25.cpp
+++ DSA/Day_47/lec_25.cpp
@@ -10,9 +10,24 @@ int main()
 
     // address of operator - &
 
-    //pointers stores address
+    //pointers are used to store address
 
     cout << "Address of num is:" << &num << endl;
 
+    int *ptr =&num;
+
+    cout<<"Address is:"<<ptr<<endl;
+    cout<<"Value is:"<<*ptr<<endl;
+
+    double d = 4.3;
+    double *p2 = &d;
+
+    cout<<"Address is:"<<p2<<endl;
+    cout<<"Value is:"<<*p2<<endl;
+
+    cout<<"Size of integer is:"<<sizeof(num)<<endl;
+    cout<<"Size of pointer is:"<<sizeof(ptr)<<endl;
+
+    cout<<"Size of pointer is:"<<sizeof(p2)<<endl;
     return 0;
 }
