57095f0c93cbd6a609ef94b89c81e48edfe28727
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-02 19:38:09 +0530

    .

---
+++ DSA/Day_47/lec_25.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int num = 5;
+
+    cout << num << endl;
+
+    // address of operator - &
+
+    //pointers stores address
+
+    cout << "Address of num is:" << &num << endl;
+
+    return 0;
+}
