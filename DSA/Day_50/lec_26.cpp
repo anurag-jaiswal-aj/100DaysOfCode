c6d18ea1ff0cf68d282480dc2e9c6df700e0ee49
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-12 13:41:39 +0530

    .

--- DSA/Day_50/lec_26.cpp
+++ DSA/Day_50/lec_26.cpp
@@ -1,4 +1,4 @@
-// -----------------------Pointers array-----------------------
+// -----------------------Pointers_array-----------------------
 
 // #include<iostream>
 // using namespace std;
@@ -64,6 +64,81 @@
 
 
 
-// -------------------------------pointers character array------------------
+// -------------------------------pointers_character array------------------
 
 
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+
+//     int arr[5] = {1,2,3,4,5};
+//     char ch[6] = "abcde";
+
+
+//     cout << arr << endl;
+//     //attention here
+//     cout << ch << endl;
+    
+//     char *c = &ch[0];
+//     //prints entire string
+//     cout << c << endl;
+
+//     char temp = 'z';
+//     char *p = &temp;
+
+//     cout << p << endl;
+
+
+//     return 0;
+// }
+
+
+// -------------------Pointers_function------------------------
+
+
+#include<iostream>
+using namespace std;
+
+void print(int *p) {
+
+    cout << *p << endl; 
+
+}
+
+void update(int *p) {
+
+   // p = p + 1;
+   //cout << "inside "<< p <<endl;
+   *p = *p + 1;
+
+}
+
+int getSum(int *arr, int n) {
+
+    cout << endl << "Size : " << sizeof(arr) << endl;
+
+    int sum = 0;
+    for(int i=0;i<n;i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+/*
+    int value = 5;
+    int *p = &value;
+
+    //print(p);
+    cout <<" Before " << *p << endl;
+    update(p);
+    cout <<" After " << *p << endl;
+    */
+
+    int arr[6] = {1,2,3,4,5,8};
+
+    cout << "Sum is " << getSum(arr+3 ,3) << endl ;  
+
+    return 0;
+}
