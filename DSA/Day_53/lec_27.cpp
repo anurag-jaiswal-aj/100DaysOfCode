a9b0cbfdef6df9333d81ba827c950fa3f4170551
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-13 18:49:02 +0530

    .

--- DSA/Day_53/lec_27.cpp
+++ DSA/Day_53/lec_27.cpp
@@ -3,6 +3,30 @@ using namespace std;
 
 int main(){
 
+    int i = 5;
+    int* p = &i;
+    int** p2 = &p;
+
+    cout<<endl<<"OK"<<endl;
+
+    cout<<"Printing p: "<<p<<endl;
+    cout<<"Printing address of p: "<<&p<<endl;
+
+    cout<<"*p2: "<<*p2<<endl;
+
+
+    cout<< i <<endl;
+    cout<< *p <<endl;
+    cout<< **p2 <<endl;
+
+    cout<<&i<<endl;
+    cout<<p<<endl;
+    cout<<*p2<<endl;
+
+    cout<<&p<<endl;
+    cout<<p2<<endl;
+
+
 
 
     return 0;
