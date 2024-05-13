ef4248dbf7720094f175e64fbd2a6ffc29a3d8f7
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-13 19:04:06 +0530

    .

--- DSA/Day_53/lec_27.cpp
+++ DSA/Day_53/lec_27.cpp
@@ -1,5 +1,21 @@
+
+/*
 #include <iostream>
 using namespace std;
+
+void update(int **p2){
+    // p2 = p2 +1;
+    //kuch change hoga kya -----no
+
+    // *p2 = *p2 + 1;
+    //kuch change hoga kya -----yes
+
+
+    **p2 = **p2 + 1;
+    //kuch change hoga kya -----yes
+
+}
+
 
 int main(){
 
@@ -7,24 +23,50 @@ int main(){
     int* p = &i;
     int** p2 = &p;
 
-    cout<<endl<<"OK"<<endl;
+    // cout<<endl<<"OK"<<endl;
 
-    cout<<"Printing p: "<<p<<endl;
-    cout<<"Printing address of p: "<<&p<<endl;
+    // cout<<"Printing p: "<<p<<endl;
+    // cout<<"Printing address of p: "<<&p<<endl;
 
-    cout<<"*p2: "<<*p2<<endl;
+    // cout<<"*p2: "<<*p2<<endl;
 
 
-    cout<< i <<endl;
-    cout<< *p <<endl;
-    cout<< **p2 <<endl;
+    // cout<< i <<endl;
+    // cout<< *p <<endl;
+    // cout<< **p2 <<endl;
 
-    cout<<&i<<endl;
-    cout<<p<<endl;
-    cout<<*p2<<endl;
+    // cout<<&i<<endl;
+    // cout<<p<<endl;
+    // cout<<*p2<<endl;
 
-    cout<<&p<<endl;
-    cout<<p2<<endl;
+    // cout<<&p<<endl;
+    // cout<<p2<<endl;
+
+
+cout<<endl;
+cout<<"Before"<<i <<endl;
+cout<<"Before"<<p <<endl;
+cout<<"Before"<<p2 <<endl;
+update(p2);
+cout<<"After"<<i<<endl;
+cout<<"After"<<p<<endl;
+cout<<"After"<<p2<<endl;
+
+
+
+    return 0;
+}
+
+*/
+
+
+
+// ---------------practice problems----------------
+
+#include <iostream>
+using namespace std;
+
+int main(){
 
 
 
