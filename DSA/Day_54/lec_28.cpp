a9b548261d0619a6429b9c9f6c6011a10e9cea8f
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-14 23:47:23 +0530

    .

--- DSA/Day_54/lec_28.cpp
+++ DSA/Day_54/lec_28.cpp
@@ -1,47 +1,68 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+
+// int& func(int a){
+//     int num = a;
+//     int& ans = num;
+//     return ans;
+// }
+
+// int* func1(int n){
+//     int* ptr =&n;
+//     return ptr;
+// }
+
+// void update2(int& n){
+//     n++;
+// }
+
+// void update1(int n){
+//     n++;
+// }
+
+
+// int main(){
+
+//     /*
+//     int i =5;
+//     int& j = i;
+//     cout<<i<<endl;
+//     i++;
+//     cout<<i<<endl;
+//     j++;
+//     cout<<i<<endl;
+//     */
+
+//    int n = 5;
+//    cout<<"Before:"<<n<<endl;
+//    update2(n);  //pass by reference (new memory will not be created)
+//    cout<<"After:"<<n<<endl;
+
+//    func(n);
+//    func1(n);
+
+
+
+//     return 0;
+// }
+
+
+
+
+
+
+// -----------------------
+
+#include<iostream>
 using namespace std;
-
-
-int& func(int a){
-    int num = a;
-    int& ans = num;
-    return ans;
-}
-
-int* func1(int n){
-    int* ptr =&n;
-    return ptr;
-}
-
-void update2(int& n){
-    n++;
-}
-
-void update1(int n){
-    n++;
-}
-
-
 int main(){
 
-    /*
-    int i =5;
-    int& j = i;
-    cout<<i<<endl;
-    i++;
-    cout<<i<<endl;
-    j++;
-    cout<<i<<endl;
-    */
+    char ch = 'q';
+    cout<< sizeof(ch) <<endl;
 
-   int n = 5;
-   cout<<"Before:"<<n<<endl;
-   update2(n);  //pass by reference (new memory will not be created)
-   cout<<"After:"<<n<<endl;
-
-   func(n);
-   func1(n);
-
+    char* c = &ch;
+    cout<<sizeof(c)<<endl;
 
     return 0;
 }
