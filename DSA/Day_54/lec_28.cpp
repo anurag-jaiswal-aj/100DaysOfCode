378cb657c1558e05c090b836bab7f92c8e8ef904
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-15 00:31:21 +0530

    .

--- DSA/Day_54/lec_28.cpp
+++ DSA/Day_54/lec_28.cpp
@@ -54,15 +54,50 @@
 
 // -----------------------
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+//     char ch = 'q';
+//     cout<< sizeof(ch) <<endl;
+
+//     char* c = &ch;
+//     cout<<sizeof(c)<<endl;
+
+//     return 0;
+// }
+
+
+
+
+// -------------
 #include<iostream>
 using namespace std;
+
+int getSum(int *arr,int n){
+    int sum  =0 ;
+    for(int  i=0;i<n;i++){
+        sum += arr[i];
+    }
+    return sum;
+}
+
 int main(){
+    int n;
+    cin>> n;
 
-    char ch = 'q';
-    cout<< sizeof(ch) <<endl;
+    //variable size array
+    int* arr = new int[n];
 
-    char* c = &ch;
-    cout<<sizeof(c)<<endl;
+    //taking input in array
+    for(int i =0;i<n;i++){
+        cin>> arr[i];
+    }
+
+    int ans = getSum(arr,n);
+    cout<<"Answer is: "<< ans<<endl;
+
+
 
     return 0;
 }
