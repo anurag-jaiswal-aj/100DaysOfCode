15926978935b1c30350e80d4e5446df34520f895
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-14 19:50:20 +0530

    .

--- DSA/Day_54/lec_28.cpp
+++ DSA/Day_54/lec_28.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 using namespace std;
 
+
+void update2(int& n){
+    n++;
+}
+
+void update1(int n){
+    n++;
+}
+
+
 int main(){
 
+    /*
+    int i =5;
+    int& j = i;
+    cout<<i<<endl;
+    i++;
+    cout<<i<<endl;
+    j++;
+    cout<<i<<endl;
+    */
 
+   int n = 5;
+   cout<<"Before:"<<n<<endl;
+   update2(n);  //pass by reference (new memory will not be created)
+   cout<<"After:"<<n<<endl;
 
     return 0;
 }
