25a1d98b584b7c02b745de32f51fd6f4ad16b283
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-16 17:55:23 +0530

    .

--- DSA/Day_56/lec_29.cpp
+++ DSA/Day_56/lec_29.cpp
@@ -2,7 +2,36 @@
 using namespace std;
 
 int main(){
+    int row;
+    cout<<"Enter the number of rows:"<<endl;
+    cin>>row;
 
+    int col;
+    cout<<"Enter the number of column:"<<endl;
+    cin>>col;
+
+    //creation 2d array
+    int** arr = new int*[row];
+    for(int i = 0;i<row;i++){
+        arr[i] = new int[col];
+    }
+    
+    //taking input
+    cout<<"Enter the elements of array:"<<endl;
+    for(int i =0;i<row;i++){
+        for(int j = 0;j<col;j++){
+            cin>> arr[i][j];
+        }
+    }
+    cout<<endl;
+
+    //taking output
+    for(int i =0;i<row;i++){
+        for(int j = 0;j<col;j++){
+            cout<< arr[i][j]<< " ";
+        }
+        cout<<endl;
+    }
 
 
     return 0;
