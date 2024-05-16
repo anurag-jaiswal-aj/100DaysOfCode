aa3d5b3e3001164f88d5b4a41dbf2afb55d01eb4
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-16 21:53:46 +0530

    .

--- DSA/Day_57/lec_30.cpp
+++ DSA/Day_57/lec_30.cpp
@@ -13,12 +13,10 @@
 //     return 0;
 // }
 
-
 //----------------------global variable------
 
 // #include <iostream>
 // using namespace std;
-
 
 // void a(int& i){
 //     cout<<i<<endl;
@@ -33,27 +31,71 @@
 //     int i = 5;
 //     a(i);
 
+//     return 0;
+// }
+
+// --------inline function-----------------
+
+// #include <iostream>
+// using namespace std;
+
+// void func(int a,int b){
+//     a++;
+//     b++;
+//     cout<<a<<" "<<b<<endl;
+// }
+
+// int main(){
+//     int a =1,b = 2;
+//     func(a,b);
 
 //     return 0;
 // }
 
 
 
-// --------inline function-----------------
 
-#include <iostream>
+
+// #include <iostream>
+// using namespace std;
+
+// inline int getMax(int &a, int &b)
+// {
+//     return (a > b) ? a : b;
+// }
+
+// int main()
+// {
+
+//     int a = 1, b = 2;
+//     int ans = 0;
+
+//     ans = getMax(a, b);
+//     cout<<ans<<endl;
+
+//     a = a + 3;
+//     b = b + 1;
+//     ans = getMax(a, b);
+//     cout<<ans<<endl;
+
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+// ---------------Default argument------------
+
+#include<iostream>
 using namespace std;
 
-
-void func(int a,int b){
-    a++;
-    b++;
-    cout<<a<<" "<<b<<endl;
-}
-
 int main(){
-    int a =1,b = 2;
-    func(a,b);
+
+
 
     return 0;
 }
