df16087697c783de9107db8fed31eae58aad5b2b
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-16 18:05:58 +0530

    .

--- DSA/Day_56/lec_29.cpp
+++ DSA/Day_56/lec_29.cpp
@@ -1,38 +1,51 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int row;
-    cout<<"Enter the number of rows:"<<endl;
-    cin>>row;
+    cout << "Enter the number of rows:" << endl;
+    cin >> row;
 
     int col;
-    cout<<"Enter the number of column:"<<endl;
-    cin>>col;
+    cout << "Enter the number of column:" << endl;
+    cin >> col;
 
-    //creation 2d array
-    int** arr = new int*[row];
-    for(int i = 0;i<row;i++){
+    // creation 2d array
+    int **arr = new int *[row];
+    for (int i = 0; i < row; i++)
+    {
         arr[i] = new int[col];
     }
-    
-    //taking input
-    cout<<"Enter the elements of array:"<<endl;
-    for(int i =0;i<row;i++){
-        for(int j = 0;j<col;j++){
-            cin>> arr[i][j];
+
+    // taking input
+    cout << "Enter the elements of array:" << endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> arr[i][j];
         }
     }
-    cout<<endl;
+    cout << endl;
 
-    //taking output
-    for(int i =0;i<row;i++){
-        for(int j = 0;j<col;j++){
-            cout<< arr[i][j]<< " ";
+    // taking output
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
+    // releasing memory
+    for (int i = 0; i < row; i++)
+    {
+        delete[] arr[i];
+    }
+
+    delete[] arr;
 
     return 0;
 }
