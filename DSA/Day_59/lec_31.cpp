1d3485af717ed323750a821d0814b3942f32f7f7
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-17 18:18:39 +0530

    .

--- DSA/Day_59/lec_31.cpp
+++ DSA/Day_59/lec_31.cpp
@@ -7,10 +7,16 @@ using namespace std;
 int factorial(int n){
     if(n == 0)
         return 1;
+
+        // int smallerProblem = factorial(n-1);
+        // int biggerProblem  = n * smallerProblem;
+        // return biggerProblem;
+
     return n * factorial(n-1);
 }
 int main(){
     int n ;
+    cout<<"Enter the value of n:"<<endl;
     cin>> n;
 
     int ans = factorial(n);
