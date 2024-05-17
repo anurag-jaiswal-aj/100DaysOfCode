461c842f802269e6fe794e7e13ee60773db25052
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-17 23:08:39 +0530

    .

--- DSA/Day_60/lec_32.cpp
+++ DSA/Day_60/lec_32.cpp
@@ -120,10 +120,39 @@
 
 // --------------say digit------------
 
-#include <iostream>
+#include<iostream> 
 using namespace std;
-int main(){
 
+void sayDigit(int n, string arr[]) {
+
+    //base case
+    if(n == 0)
+        return ;
+
+    //processing 
+    int digit = n % 10;
+    n = n / 10;
+    
+
+    //recursive call
+    sayDigit(n, arr);
+
+    cout << arr[digit] << " ";
+
+}
+
+int main() {
+
+    string arr[10] = {"zero", "one", "two", "three",
+                         "four", "five", "six"
+                        , "seven", "eight", "nine"};
+    int n;
+    cout<<"Enter the value of n:"<<endl;
+    cin >> n;
+
+    cout << endl;
+    sayDigit(n, arr);
+    cout << endl<<endl;
 
 
     return 0;
