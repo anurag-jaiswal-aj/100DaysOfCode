75901e3833f289de70bdc1cfaddad69d6b1a6c66
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-17 20:12:29 +0530

    .

--- DSA/Day_60/lec_32.cpp
+++ DSA/Day_60/lec_32.cpp
@@ -1,9 +1,70 @@
+
+
+// #include <iostream>
+// using namespace std;
+
+// void reachHome(int src, int dest)
+// {
+
+//     cout << "Source:" << src << "   Destination:" << dest << endl;
+
+//     // base case
+
+//     if (src == dest)
+//     {
+//         cout << "Reached destination" << endl;
+//         return;
+//     }
+
+//     // processing - one step aage badhjao
+//     src++;
+
+//     // rcursion call
+//     reachHome(src, dest);
+// }
+
+// int main()
+// {
+
+//     int dest = 10;
+//     int src = 1;
+
+//     cout << endl;
+
+//     reachHome(src, dest);
+
+//     return 0;
+// }
+
+
+// -----------------------febonaci series------------
+
 #include <iostream>
 using namespace std;
 
+int fibonacci(int n){
+    if(n == 0){
+        return 0;
+    }
+    if(n == 1){
+        return 1;
+    }
+
+    return fibonacci(n-1) + fibonacci(n-2);
+
+}
+
+
 int main(){
+    int n;
+    cout<<"Enter the value of n:"<<endl;
+    cin>>n;
 
-
+    cout<<"Fibonacci series is:"<<endl;
+    for (int i = 0; i < n; i++) {
+        cout << fibonacci(i) << " ";
+    }
+    cout << endl;
 
     return 0;
 }
