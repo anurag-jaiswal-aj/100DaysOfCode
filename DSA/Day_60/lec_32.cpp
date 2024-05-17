b5f4b0e751a4f6593f19263c0e87dc2d5cacf30c
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-17 20:17:36 +0530

    .

--- DSA/Day_60/lec_32.cpp
+++ DSA/Day_60/lec_32.cpp
@@ -36,35 +36,39 @@
 //     return 0;
 // }
 
-
 // -----------------------febonaci series------------
 
 #include <iostream>
 using namespace std;
 
-int fibonacci(int n){
-    if(n == 0){
+int fibonacci(int n)
+{
+
+    // base case
+    if (n == 0)
+    {
         return 0;
     }
-    if(n == 1){
+    if (n == 1)
+    {
         return 1;
     }
 
-    return fibonacci(n-1) + fibonacci(n-2);
-
+    // recursion call
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-
-int main(){
+int main()
+{
     int n;
-    cout<<"Enter the value of n:"<<endl;
-    cin>>n;
+    cout << "Enter the value of n:" << endl;
+    cin >> n;
 
-    cout<<"Fibonacci series is:"<<endl;
-    for (int i = 0; i < n; i++) {
+    cout << "Fibonacci series is:" << endl;
+    for (int i = 0; i < n; i++)
+    {
         cout << fibonacci(i) << " ";
     }
     cout << endl;
-
     return 0;
 }
