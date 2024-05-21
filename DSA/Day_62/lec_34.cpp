ccd755ca229f806a69f2b2076327c44fc42a1cef
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-22 00:49:15 +0530

    .

--- DSA/Day_62/lec_34.cpp
+++ DSA/Day_62/lec_34.cpp
@@ -1,11 +1,77 @@
 
-// ----------Reverse string using recursion-----------
+// // ----------Reverse string using recursion-----------
+
+// #include <iostream>
+// using namespace std;
+
+
+// void reverse(string& str,int i,int j){
+
+//     cout<<"Call recieved for " << str<<endl;
+
+//     //base case
+//     if(i>j){
+//         return ;
+//     }
+
+//     swap(str[i],str[j]);
+//     i++;
+//     j--;
+
+//     //recursive call 
+//     reverse(str,i,j);
+// }
+
+// int main(){
+
+//     string name = "abcde";
+//     cout<<endl;
+//     reverse(name,0,name.length() -1);
+//     cout<<endl;
+//     cout<<name<<endl;
+
+//     return 0;
+// }
+
+
+
+
+
+
+//-----------------------palindrome check-----------
 
 #include <iostream>
 using namespace std;
 
-int main(){
 
+bool checkPalindrome(string str,int i , int j){
+
+    //base case
+    if(i>j){
+        return true;
+    }
+
+    if(str[i] != str[j]){
+        return false;
+    }
+    else{
+        //recursie call  
+        return checkPalindrome(str,1+1,j-1);
+    }
+}
+
+int main(){
+    string name = "BookkooB";
+    cout << endl;
+
+    bool isPalindrome = checkPalindrome(name, 0, name.length()-1 );
+
+    if(isPalindrome) {
+        cout << "Its a Palindrome " << endl; 
+    }
+    else {
+        cout << "Its not a Palindrome " << endl;
+    }
 
 
 
