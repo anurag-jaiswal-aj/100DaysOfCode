ebae5d7e2cb03bc1084d0712b3ba8d1608cb3890
Anurag Jaiswal <janurag582004@gmail.com>
2024-05-22 00:51:58 +0530

    .

--- DSA/Day_62/lec_34.cpp
+++ DSA/Day_62/lec_34.cpp
@@ -40,39 +40,118 @@
 
 //-----------------------palindrome check-----------
 
-#include <iostream>
+
+// #include<iostream>
+// using namespace std;
+
+// bool checkPalindrome(string str, int i, int  j) {
+
+//     //base case
+//     if(i>j)
+//         return true;
+
+//     if(str[i] != str[j])
+//         return false;
+//     else{
+//         //Recursive call
+//         return checkPalindrome(str, i+1,j-1);
+//     }
+// }
+
+// int main() {
+
+//     string name = "BookkooB";
+//     cout << endl;
+
+//     bool isPalindrome = checkPalindrome(name, 0, name.length()-1 );
+
+//     if(isPalindrome) {
+//         cout << "Its a Palindrome " << endl; 
+//     }
+//     else {
+//         cout << "Its not a Palindrome " << endl;
+//     }
+
+
+//     return 0;
+// }
+
+
+
+
+// -----------------exponent----------------
+
+// #include<iostream>
+// using namespace std;
+
+// int power(int a, int b) {
+//     //base case
+//     if( b == 0 )
+//         return 1;
+
+//     if(b == 1)
+//         return a;
+
+//         //recutrsive call
+//     int ans = power(a, b/2);
+
+//     //if b is even
+//     if(b%2 == 0) {
+//         return ans * ans;
+//     }
+//     else {
+//         //if b is odd
+//         return a * ans * ans;
+//     }
+// }
+
+// int main() {
+
+//     int a,b;
+//     cin >> a >> b;
+//     cout << endl;
+//     int ans = power(a,b);
+
+//     cout << "Answer is " << ans << endl;
+
+//     return 0;
+// }
+
+
+
+// ---------bubble sort----------------------
+
+#include<iostream>
 using namespace std;
 
+void sortArray(int *arr, int n) {
 
-bool checkPalindrome(string str,int i , int j){
-
-    //base case
-    if(i>j){
-        return true;
+    //base case - already sorted
+    if(n == 0 || n == 1) {
+        return ;
     }
 
-    if(str[i] != str[j]){
-        return false;
+    //1 case sovle karlia - largest element ko end me rakh dega
+    for(int i=0; i<n-1; i++) {
+        if(arr[i] > arr[i+1]){
+            swap(arr[i], arr[i+1]);
+        }
     }
-    else{
-        //recursie call  
-        return checkPalindrome(str,1+1,j-1);
-    }
+
+    //Recursive Call
+    sortArray(arr, n-1);
+
 }
 
-int main(){
-    string name = "BookkooB";
-    cout << endl;
+int main() {
 
-    bool isPalindrome = checkPalindrome(name, 0, name.length()-1 );
+    int arr[5] = {2,5,1,6,9};
+    
+    sortArray(arr,5);
 
-    if(isPalindrome) {
-        cout << "Its a Palindrome " << endl; 
-    }
-    else {
-        cout << "Its not a Palindrome " << endl;
-    }
-
+    for(int i=0; i<5; i++) {
+        cout << arr[i] << " ";
+    }cout << endl;
 
 
     return 0;
